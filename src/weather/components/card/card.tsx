e2fd9ec3f028acc455fe39596d2c11e93e106a6d
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-05-11 17:43:43 -0300

    feat: update weather card

--- src/weather/components/card/card.tsx
+++ src/weather/components/card/card.tsx
@@ -1,29 +1,82 @@
+import { IoLocationSharp } from "react-icons/io5";
+import {
+  FaTemperatureHalf,
+  FaTemperatureArrowDown,
+  FaTemperatureArrowUp,
+  FaDroplet,
+} from "react-icons/fa6";
+
 import {
   WeatherCardWrapper,
+  MainInfoWrapper,
   CurrentTemperature,
-  MoreInfoWrapper,
-  MaxTemperature,
   Description,
+  CityName,
+  ExtraInfoWrapper,
+  ExtraInfoCard,
+  IconWrapper,
 } from "@/weather/components/card/card.styles";
 
 type WeatherCardProps = {
   temp: string;
-  tempmax: string;
+  maxtemp: string;
+  mintemp: string;
+  fellsliketemp: string;
+  cityname: string;
   description: string;
+  humidity: string;
+  icon: string;
 };
 
 export const WeatherCard = ({
   temp,
-  tempmax,
+  maxtemp,
+  mintemp,
+  fellsliketemp,
+  cityname,
   description,
+  humidity,
+  icon,
 }: WeatherCardProps) => {
+  const weatherIconSrc = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
   return (
     <WeatherCardWrapper>
-      <CurrentTemperature>{temp}</CurrentTemperature>
-      <MoreInfoWrapper>
+      <MainInfoWrapper>
+        <img src={weatherIconSrc} />
+        <CurrentTemperature>{temp}</CurrentTemperature>
         <Description>{description}</Description>
-        <MaxTemperature>Max Temp: {tempmax}</MaxTemperature>
-      </MoreInfoWrapper>
+        <IconWrapper>
+          <IoLocationSharp />
+          <CityName>{cityname}</CityName>
+        </IconWrapper>
+      </MainInfoWrapper>
+      <ExtraInfoWrapper>
+        <ExtraInfoCard title="Temperatura minima">
+          <IconWrapper>
+            <FaTemperatureArrowDown />
+            <p>{mintemp}</p>
+          </IconWrapper>
+        </ExtraInfoCard>
+        <ExtraInfoCard title="Temperatura maxima">
+          <IconWrapper>
+            <FaTemperatureArrowUp />
+            <p>{maxtemp}</p>
+          </IconWrapper>
+        </ExtraInfoCard>
+        <ExtraInfoCard title="Sensação Térmica">
+          <IconWrapper>
+            <FaTemperatureHalf />
+            <p>{fellsliketemp}</p>
+          </IconWrapper>
+        </ExtraInfoCard>
+        <ExtraInfoCard title="Umidade">
+          <IconWrapper>
+            <FaDroplet />
+            <p>{humidity}</p>
+          </IconWrapper>
+        </ExtraInfoCard>
+      </ExtraInfoWrapper>
     </WeatherCardWrapper>
   );
 };
