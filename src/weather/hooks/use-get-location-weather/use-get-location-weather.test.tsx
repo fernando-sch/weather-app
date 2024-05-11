9c4e920aa6c7a1a54037534e8df7b77917cbc60a
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-05-11 15:55:49 -0300

    chore: refactor and update get city location and weather hooks

--- src/weather/hooks/use-get-location-weather/use-get-location-weather.test.tsx
+++ src/weather/hooks/use-get-location-weather/use-get-location-weather.test.tsx
@@ -1,9 +1,6 @@
 import { describe, expect, it } from "vitest";
 import { waitFor } from "@testing-library/react";
-import {
-  renderHookWithQueryProvider,
-  startMockServer,
-} from "@/__tests__/helpers";
+import { renderCustomHook, startMockServer } from "@/__tests__/helpers";
 import { useGetLocationWeather } from "@/weather/hooks/use-get-location-weather";
 
 describe("useGetLocationWeather", () => {
@@ -17,10 +14,16 @@ describe("useGetLocationWeather", () => {
     server.shutdown();
   });
 
+  it("shouldn't fetch data if lat and lon are undefined", () => {
+    const { result } = renderCustomHook(useGetLocationWeather);
+
+    expect(result.current.data).toBeUndefined();
+    expect(result.current.fetchStatus).toBe("idle");
+  });
+
   it("should return the location weather", async () => {
-    const { result } = renderHookWithQueryProvider(() =>
-      useGetLocationWeather(-25.4295963, -49.2712724)
-    );
+    const [lat, lon] = ["-25.4295963", "-49.2712724"];
+    const { result } = renderCustomHook(() => useGetLocationWeather(lat, lon));
 
     await waitFor(() => {
       expect(result.current.isSuccess).toBe(true);
