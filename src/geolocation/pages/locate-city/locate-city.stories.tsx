fab174f30a353ed2ed1cc3c290a3549ea2ec1687
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-05-11 15:54:49 -0300

    chore: update locate city page to use correct component and test helper

--- src/geolocation/pages/locate-city/locate-city.stories.tsx
+++ src/geolocation/pages/locate-city/locate-city.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta, StoryObj } from "@storybook/react";
+import { MemoryRouter } from "react-router-dom";
 import { LocateCityPage } from "@/geolocation/pages/locate-city";
 
 const meta: Meta<typeof LocateCityPage> = { component: LocateCityPage };
@@ -6,4 +7,10 @@ const meta: Meta<typeof LocateCityPage> = { component: LocateCityPage };
 export default meta;
 type Story = StoryObj<typeof LocateCityPage>;
 
-export const Default: Story = { render: () => <LocateCityPage /> };
+export const Default: Story = {
+  render: () => (
+    <MemoryRouter>
+      <LocateCityPage />
+    </MemoryRouter>
+  ),
+};
--- src/geolocation/pages/locate-city/locate-city.test.tsx
+++ src/geolocation/pages/locate-city/locate-city.test.tsx
@@ -1,11 +1,11 @@
 import { describe, expect, it } from "vitest";
 import { screen } from "@testing-library/react";
 import { LocateCityPage } from "@/geolocation/pages/locate-city";
-import { renderComponentWithQueryProvider } from "@/__tests__/helpers";
+import { renderComponent } from "@/__tests__/helpers";
 
 describe("LocateCity", () => {
   it("should render component", () => {
-    renderComponentWithQueryProvider(<LocateCityPage />);
+    renderComponent(<LocateCityPage />);
 
     expect(screen.getByText(/Weather App/)).toBeDefined();
     expect(screen.getByPlaceholderText(/Nome da cidade/)).toBeDefined();
