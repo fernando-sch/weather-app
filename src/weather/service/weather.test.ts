05b335321c553bec89035e4bec30e59a2b219d22
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-05-11 22:51:25 -0300

    chore: add icon src property on weather dto

--- src/weather/service/weather.test.ts
+++ src/weather/service/weather.test.ts
@@ -23,10 +23,10 @@ describe("WeatherService", () => {
       expect(data.name).toBe("Curitiba");
       expect(data.weather[0].description).toBe("nublado");
       expect(data.weather[0].icon).toBe("04n");
-      expect(data.main.temp).toBe(22.18);
-      expect(data.main.feels_like).toBe(22.26);
-      expect(data.main.temp_min).toBe(20.05);
-      expect(data.main.temp_max).toBe(24.1);
+      expect(data.main.temp).toBe(23);
+      expect(data.main.feels_like).toBe(22);
+      expect(data.main.temp_min).toBe(20);
+      expect(data.main.temp_max).toBe(24);
       expect(data.main.humidity).toBe(69);
     });
   });
