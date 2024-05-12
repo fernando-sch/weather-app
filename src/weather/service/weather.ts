7bdc8e8faee28807dfef05823fca8ea3d5b858c7
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-05-12 15:05:18 -0300

    chore: add new ibge api env and axios instance

--- src/weather/service/weather.ts
+++ src/weather/service/weather.ts
@@ -1,9 +1,9 @@
 import { AxiosInstance, AxiosResponse } from "axios";
-import { axiosInstance, envObject } from "@/config";
+import { openWeatherAxiosInstance, envObject } from "@/config";
 import { RemoteWeather } from "@/weather/dto";
 
 export class WeatherService {
-  #axiosIntance: AxiosInstance = axiosInstance;
+  #axiosInstance: AxiosInstance = openWeatherAxiosInstance;
 
   async getLocationWeather(
     lat?: string,
@@ -11,6 +11,6 @@ export class WeatherService {
   ): Promise<AxiosResponse<RemoteWeather>> {
     const defaultParams = { units: "metric", lang: "pt_br" };
     const params = { lat, lon, appid: envObject.apiKey, ...defaultParams };
-    return await this.#axiosIntance.get("/data/2.5/weather", { params });
+    return await this.#axiosInstance.get("/data/2.5/weather", { params });
   }
 }
